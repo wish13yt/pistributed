3e2c328d0c475b611d40ec1d8211beb2abc997e4
Wish <wishyt@proton.me>
2025-09-30 18:42:03 -0500

    greg

--- src/client/client.py
+++ src/client/client.py
@@ -1,6 +1,18 @@
 from math import factorial
 from decimal import Decimal, getcontext
+print("Pistributed, an app to calculate Pi with distributed power.")
+print("Do not run on systems owned by another entity, entities, person, or people, without explicit permission from them.")
+print("""This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.""")
+print("By continuing, you agree to these terms, disclaimers, and acknowledge that there is NO WARRANTY.")
+input("I agree, and would like to continue. (to continue, press enter)")
+print("Thanks for confirming! I just need one more thing.")
+server = input("What is the server URL that you'd like to connect to? (ex: example.com) ")
+print("Alright! Thanks. Let the calculating begin :D (to exit, run ctrl+c or close terminal)")
+print("-------------------")
 getcontext().prec=100
 def calc(n):
     t= Decimal(0)
