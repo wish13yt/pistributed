37ad81e66a538e5a4558ee4f51b1e15ff34a103e
Wish <wishyt@proton.me>
2025-09-30 19:03:05 -0500

    add lander soon

--- src/server/app.py
+++ src/server/app.py
@@ -7,7 +7,7 @@ load_dotenv()
 name = os.getenv("NAME")
 email = os.getenv("EMAIL")
 
-@app.route('/api/getpi', methods=['GET'])
+@app.route('/api/getpi')
 def getpi():
     if request.method == 'GET':
         try:
@@ -16,4 +16,18 @@ def getpi():
         except:
             return "picalc/pi.txt wasn't found! Contact " + name + " at " + email + " if this persists."
     else:
-        return "Invaild method! /getpi is designed for GET requests."
+        return "Invaild method! /getpi is designed for GET requests, not " + request.method
+    
+@app.route('/api/postpi')
+def postpi():
+    if request.method == 'POST':
+        return "Not Implemented"
+    else:
+        return "Invalid method! /postpi is designed for POST requests, not " + request.method
+
+@app.route('/')
+def lander():
+    if request.method == 'GET':
+        return "Not implemented"
+    else:
+        return "Please use GET to access this page."
