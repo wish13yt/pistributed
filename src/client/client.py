6fdea7863ea1bce52a5bbec2977db56ddc63f8bd
Wish <wishyt@proton.me>
2025-10-02 17:42:08 -0500

    add saving pi_val on error

--- src/client/client.py
+++ src/client/client.py
@@ -3,6 +3,7 @@ from decimal import Decimal, getcontext
 import time
 import threading
 import requests
+import sys
 
 print("Pistributed, an app to calculate Pi with distributed power.")
 print("Do not run on systems owned by another entity, entities, person, or people, without explicit permission from them.")
@@ -20,53 +21,63 @@ print("Alright! Thanks. Let the calculating begin :D (to exit, run ctrl+c or clo
 print("-------------------")
 global pi_val
 pi_val = None
-def calc(n, prec):
-    getcontext().prec = prec
-    t = Decimal(0)
-    pi = Decimal(0)
-    deno = Decimal(0)
-    for k in range(n):
-        t = ((-1)**k) * (factorial(6*k)) * (13591409 + 545140134*k)
-        deno = factorial(3*k) * (factorial(k)**3) * (640320**(3*k))
-        pi += Decimal(t) / Decimal(deno)
-    pi = pi * Decimal(12) / Decimal(640320**Decimal(1.5))
-    pi = 1/pi
-    return pi
+try:
+    def calc(n, prec):
+        getcontext().prec = prec
+        t = Decimal(0)
+        pi = Decimal(0)
+        deno = Decimal(0)
+        for k in range(n):
+            t = ((-1)**k) * (factorial(6*k)) * (13591409 + 545140134*k)
+            deno = factorial(3*k) * (factorial(k)**3) * (640320**(3*k))
+            pi += Decimal(t) / Decimal(deno)
+        pi = pi * Decimal(12) / Decimal(640320**Decimal(1.5))
+        pi = 1/pi
+        return pi
 
-def upload():
-    while True:
-        if str(pi_val).startswith("3.14") == True:
-            print(">> checking and uploading latest calculation")
-            response = requests.get(server + "/api/getpi")
-            print(response.text)
-            replen = len(response.text)
-            pilen = len(str(pi_val))
-            if response.text == "No clients have connected yet. Become one of the first!":
-                print("server pi doesn't exist! uploading")
-                data = {'pi': str(pi_val)}
-                print(data)
-                response = requests.post(server + "/api/postpi", data=data)
+    def upload():
+        while True:
+            if str(pi_val).startswith("3.14") == True:
+                print(">> checking and uploading latest calculation")
+                response = requests.get(server + "/api/getpi")
                 print(response.text)
-            else:
-                if replen > pilen:
-                    print("server pi length is greater than local pi length! not uploading")
-                elif pilen > replen:
-                    print("local pi length longer than server pi length! uploading")
+                replen = len(response.text)
+                pilen = len(str(pi_val))
+                if response.text == "No clients have connected yet. Become one of the first!":
+                    print("server pi doesn't exist! uploading")
                     data = {'pi': str(pi_val)}
+                    print(data)
                     response = requests.post(server + "/api/postpi", data=data)
                     print(response.text)
-        else:
-            print("pi_val not yet defined!")
-        time.sleep(60)
+                else:
+                    if replen > pilen:
+                        print("server pi length is greater than local pi length! not uploading")
+                    elif pilen > replen:
+                        print("local pi length longer than server pi length! uploading")
+                        data = {'pi': str(pi_val)}
+                        response = requests.post(server + "/api/postpi", data=data)
+                        print(response.text)
+            else:
+                print("pi_val not yet defined!")
+            time.sleep(60)
 
-threading.Thread(target=upload, daemon=True).start()
+    threading.Thread(target=upload, daemon=True).start()
 
-calculation = 1
-precision = 10
+    calculation = 1
+    precision = 10
 
-while True:
-    pi_val = calc(calculation, precision)
-    print(pi_val)
-    calculation += 1
-    precision += 5
-    time.sleep(5)
+    while True:
+        pi_val = calc(calculation, precision)
+        print(pi_val)
+        calculation += 1
+        precision += 5
+        time.sleep(5)
+except:
+    try:
+        print("Saving your latest Pi to Pi.txt and exiting...")
+        with open("pi.txt", "w") as f:
+            f.write(str(pi_val))
+        sys.exit()
+    except:
+        print("Unable to save to pi.txt. This could be because pi_val wasn't defined yet. Exiting..")
+        sys.exit()
