1decf622faa81e673cb340c52d0bf92f5490a364
Wish <wishyt@proton.me>
2025-10-08 17:55:57 -0500

    fix

--- src/client/client.py
+++ src/client/client.py
@@ -39,7 +39,7 @@ try:
             deno = factorial(3*k) * (factorial(k)**3) * (640320**(3*k))
             pi += Decimal(t) / Decimal(deno)
         pi = pi * Decimal(12) / Decimal(640320**Decimal(1.5))
-        pi = 1/pi
+        pi = 1/pi * multiplier
         return pi
 
     def upload():
